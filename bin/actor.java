90ae0c687e0b5df5c31397999dbb9ae43156914f
Deepak <tcgdigital.deepak@gmail.com>
2018-01-12 14:46:30 +0530

    REQ FILES
    ALL FILES

---
+++ bin/actor.java
@@ -0,0 +1,428 @@
+// ORM class for table 'actor'
+// WARNING: This class is AUTO-GENERATED. Modify at your own risk.
+//
+// Debug information:
+// Generated date: Sun Jun 19 22:36:51 PDT 2016
+// For connector: org.apache.sqoop.manager.MySQLManager
+import org.apache.hadoop.io.BytesWritable;
+import org.apache.hadoop.io.Text;
+import org.apache.hadoop.io.Writable;
+import org.apache.hadoop.mapred.lib.db.DBWritable;
+import com.cloudera.sqoop.lib.JdbcWritableBridge;
+import com.cloudera.sqoop.lib.DelimiterSet;
+import com.cloudera.sqoop.lib.FieldFormatter;
+import com.cloudera.sqoop.lib.RecordParser;
+import com.cloudera.sqoop.lib.BooleanParser;
+import com.cloudera.sqoop.lib.BlobRef;
+import com.cloudera.sqoop.lib.ClobRef;
+import com.cloudera.sqoop.lib.LargeObjectLoader;
+import com.cloudera.sqoop.lib.SqoopRecord;
+import java.sql.PreparedStatement;
+import java.sql.ResultSet;
+import java.sql.SQLException;
+import java.io.DataInput;
+import java.io.DataOutput;
+import java.io.IOException;
+import java.nio.ByteBuffer;
+import java.nio.CharBuffer;
+import java.sql.Date;
+import java.sql.Time;
+import java.sql.Timestamp;
+import java.util.Arrays;
+import java.util.Iterator;
+import java.util.List;
+import java.util.Map;
+import java.util.TreeMap;
+
+public class actor extends SqoopRecord  implements DBWritable, Writable {
+  private final int PROTOCOL_VERSION = 3;
+  public int getClassFormatVersion() { return PROTOCOL_VERSION; }
+  protected ResultSet __cur_result_set;
+  private Integer actor_id;
+  public Integer get_actor_id() {
+    return actor_id;
+  }
+  public void set_actor_id(Integer actor_id) {
+    this.actor_id = actor_id;
+  }
+  public actor with_actor_id(Integer actor_id) {
+    this.actor_id = actor_id;
+    return this;
+  }
+  private String first_name;
+  public String get_first_name() {
+    return first_name;
+  }
+  public void set_first_name(String first_name) {
+    this.first_name = first_name;
+  }
+  public actor with_first_name(String first_name) {
+    this.first_name = first_name;
+    return this;
+  }
+  private String last_name;
+  public String get_last_name() {
+    return last_name;
+  }
+  public void set_last_name(String last_name) {
+    this.last_name = last_name;
+  }
+  public actor with_last_name(String last_name) {
+    this.last_name = last_name;
+    return this;
+  }
+  private java.sql.Timestamp last_update;
+  public java.sql.Timestamp get_last_update() {
+    return last_update;
+  }
+  public void set_last_update(java.sql.Timestamp last_update) {
+    this.last_update = last_update;
+  }
+  public actor with_last_update(java.sql.Timestamp last_update) {
+    this.last_update = last_update;
+    return this;
+  }
+  public boolean equals(Object o) {
+    if (this == o) {
+      return true;
+    }
+    if (!(o instanceof actor)) {
+      return false;
+    }
+    actor that = (actor) o;
+    boolean equal = true;
+    equal = equal && (this.actor_id == null ? that.actor_id == null : this.actor_id.equals(that.actor_id));
+    equal = equal && (this.first_name == null ? that.first_name == null : this.first_name.equals(that.first_name));
+    equal = equal && (this.last_name == null ? that.last_name == null : this.last_name.equals(that.last_name));
+    equal = equal && (this.last_update == null ? that.last_update == null : this.last_update.equals(that.last_update));
+    return equal;
+  }
+  public boolean equals0(Object o) {
+    if (this == o) {
+      return true;
+    }
+    if (!(o instanceof actor)) {
+      return false;
+    }
+    actor that = (actor) o;
+    boolean equal = true;
+    equal = equal && (this.actor_id == null ? that.actor_id == null : this.actor_id.equals(that.actor_id));
+    equal = equal && (this.first_name == null ? that.first_name == null : this.first_name.equals(that.first_name));
+    equal = equal && (this.last_name == null ? that.last_name == null : this.last_name.equals(that.last_name));
+    equal = equal && (this.last_update == null ? that.last_update == null : this.last_update.equals(that.last_update));
+    return equal;
+  }
+  public void readFields(ResultSet __dbResults) throws SQLException {
+    this.__cur_result_set = __dbResults;
+    this.actor_id = JdbcWritableBridge.readInteger(1, __dbResults);
+    this.first_name = JdbcWritableBridge.readString(2, __dbResults);
+    this.last_name = JdbcWritableBridge.readString(3, __dbResults);
+    this.last_update = JdbcWritableBridge.readTimestamp(4, __dbResults);
+  }
+  public void readFields0(ResultSet __dbResults) throws SQLException {
+    this.actor_id = JdbcWritableBridge.readInteger(1, __dbResults);
+    this.first_name = JdbcWritableBridge.readString(2, __dbResults);
+    this.last_name = JdbcWritableBridge.readString(3, __dbResults);
+    this.last_update = JdbcWritableBridge.readTimestamp(4, __dbResults);
+  }
+  public void loadLargeObjects(LargeObjectLoader __loader)
+      throws SQLException, IOException, InterruptedException {
+  }
+  public void loadLargeObjects0(LargeObjectLoader __loader)
+      throws SQLException, IOException, InterruptedException {
+  }
+  public void write(PreparedStatement __dbStmt) throws SQLException {
+    write(__dbStmt, 0);
+  }
+
+  public int write(PreparedStatement __dbStmt, int __off) throws SQLException {
+    JdbcWritableBridge.writeInteger(actor_id, 1 + __off, 5, __dbStmt);
+    JdbcWritableBridge.writeString(first_name, 2 + __off, 12, __dbStmt);
+    JdbcWritableBridge.writeString(last_name, 3 + __off, 12, __dbStmt);
+    JdbcWritableBridge.writeTimestamp(last_update, 4 + __off, 93, __dbStmt);
+    return 4;
+  }
+  public void write0(PreparedStatement __dbStmt, int __off) throws SQLException {
+    JdbcWritableBridge.writeInteger(actor_id, 1 + __off, 5, __dbStmt);
+    JdbcWritableBridge.writeString(first_name, 2 + __off, 12, __dbStmt);
+    JdbcWritableBridge.writeString(last_name, 3 + __off, 12, __dbStmt);
+    JdbcWritableBridge.writeTimestamp(last_update, 4 + __off, 93, __dbStmt);
+  }
+  public void readFields(DataInput __dataIn) throws IOException {
+this.readFields0(__dataIn);  }
+  public void readFields0(DataInput __dataIn) throws IOException {
+    if (__dataIn.readBoolean()) { 
+        this.actor_id = null;
+    } else {
+    this.actor_id = Integer.valueOf(__dataIn.readInt());
+    }
+    if (__dataIn.readBoolean()) { 
+        this.first_name = null;
+    } else {
+    this.first_name = Text.readString(__dataIn);
+    }
+    if (__dataIn.readBoolean()) { 
+        this.last_name = null;
+    } else {
+    this.last_name = Text.readString(__dataIn);
+    }
+    if (__dataIn.readBoolean()) { 
+        this.last_update = null;
+    } else {
+    this.last_update = new Timestamp(__dataIn.readLong());
+    this.last_update.setNanos(__dataIn.readInt());
+    }
+  }
+  public void write(DataOutput __dataOut) throws IOException {
+    if (null == this.actor_id) { 
+        __dataOut.writeBoolean(true);
+    } else {
+        __dataOut.writeBoolean(false);
+    __dataOut.writeInt(this.actor_id);
+    }
+    if (null == this.first_name) { 
+        __dataOut.writeBoolean(true);
+    } else {
+        __dataOut.writeBoolean(false);
+    Text.writeString(__dataOut, first_name);
+    }
+    if (null == this.last_name) { 
+        __dataOut.writeBoolean(true);
+    } else {
+        __dataOut.writeBoolean(false);
+    Text.writeString(__dataOut, last_name);
+    }
+    if (null == this.last_update) { 
+        __dataOut.writeBoolean(true);
+    } else {
+        __dataOut.writeBoolean(false);
+    __dataOut.writeLong(this.last_update.getTime());
+    __dataOut.writeInt(this.last_update.getNanos());
+    }
+  }
+  public void write0(DataOutput __dataOut) throws IOException {
+    if (null == this.actor_id) { 
+        __dataOut.writeBoolean(true);
+    } else {
+        __dataOut.writeBoolean(false);
+    __dataOut.writeInt(this.actor_id);
+    }
+    if (null == this.first_name) { 
+        __dataOut.writeBoolean(true);
+    } else {
+        __dataOut.writeBoolean(false);
+    Text.writeString(__dataOut, first_name);
+    }
+    if (null == this.last_name) { 
+        __dataOut.writeBoolean(true);
+    } else {
+        __dataOut.writeBoolean(false);
+    Text.writeString(__dataOut, last_name);
+    }
+    if (null == this.last_update) { 
+        __dataOut.writeBoolean(true);
+    } else {
+        __dataOut.writeBoolean(false);
+    __dataOut.writeLong(this.last_update.getTime());
+    __dataOut.writeInt(this.last_update.getNanos());
+    }
+  }
+  private static final DelimiterSet __outputDelimiters = new DelimiterSet((char) 44, (char) 10, (char) 0, (char) 0, false);
+  public String toString() {
+    return toString(__outputDelimiters, true);
+  }
+  public String toString(DelimiterSet delimiters) {
+    return toString(delimiters, true);
+  }
+  public String toString(boolean useRecordDelim) {
+    return toString(__outputDelimiters, useRecordDelim);
+  }
+  public String toString(DelimiterSet delimiters, boolean useRecordDelim) {
+    StringBuilder __sb = new StringBuilder();
+    char fieldDelim = delimiters.getFieldsTerminatedBy();
+    __sb.append(FieldFormatter.escapeAndEnclose(actor_id==null?"null":"" + actor_id, delimiters));
+    __sb.append(fieldDelim);
+    __sb.append(FieldFormatter.escapeAndEnclose(first_name==null?"null":first_name, delimiters));
+    __sb.append(fieldDelim);
+    __sb.append(FieldFormatter.escapeAndEnclose(last_name==null?"null":last_name, delimiters));
+    __sb.append(fieldDelim);
+    __sb.append(FieldFormatter.escapeAndEnclose(last_update==null?"null":"" + last_update, delimiters));
+    if (useRecordDelim) {
+      __sb.append(delimiters.getLinesTerminatedBy());
+    }
+    return __sb.toString();
+  }
+  public void toString0(DelimiterSet delimiters, StringBuilder __sb, char fieldDelim) {
+    __sb.append(FieldFormatter.escapeAndEnclose(actor_id==null?"null":"" + actor_id, delimiters));
+    __sb.append(fieldDelim);
+    __sb.append(FieldFormatter.escapeAndEnclose(first_name==null?"null":first_name, delimiters));
+    __sb.append(fieldDelim);
+    __sb.append(FieldFormatter.escapeAndEnclose(last_name==null?"null":last_name, delimiters));
+    __sb.append(fieldDelim);
+    __sb.append(FieldFormatter.escapeAndEnclose(last_update==null?"null":"" + last_update, delimiters));
+  }
+  private static final DelimiterSet __inputDelimiters = new DelimiterSet((char) 44, (char) 10, (char) 0, (char) 0, false);
+  private RecordParser __parser;
+  public void parse(Text __record) throws RecordParser.ParseError {
+    if (null == this.__parser) {
+      this.__parser = new RecordParser(__inputDelimiters);
+    }
+    List<String> __fields = this.__parser.parseRecord(__record);
+    __loadFromFields(__fields);
+  }
+
+  public void parse(CharSequence __record) throws RecordParser.ParseError {
+    if (null == this.__parser) {
+      this.__parser = new RecordParser(__inputDelimiters);
+    }
+    List<String> __fields = this.__parser.parseRecord(__record);
+    __loadFromFields(__fields);
+  }
+
+  public void parse(byte [] __record) throws RecordParser.ParseError {
+    if (null == this.__parser) {
+      this.__parser = new RecordParser(__inputDelimiters);
+    }
+    List<String> __fields = this.__parser.parseRecord(__record);
+    __loadFromFields(__fields);
+  }
+
+  public void parse(char [] __record) throws RecordParser.ParseError {
+    if (null == this.__parser) {
+      this.__parser = new RecordParser(__inputDelimiters);
+    }
+    List<String> __fields = this.__parser.parseRecord(__record);
+    __loadFromFields(__fields);
+  }
+
+  public void parse(ByteBuffer __record) throws RecordParser.ParseError {
+    if (null == this.__parser) {
+      this.__parser = new RecordParser(__inputDelimiters);
+    }
+    List<String> __fields = this.__parser.parseRecord(__record);
+    __loadFromFields(__fields);
+  }
+
+  public void parse(CharBuffer __record) throws RecordParser.ParseError {
+    if (null == this.__parser) {
+      this.__parser = new RecordParser(__inputDelimiters);
+    }
+    List<String> __fields = this.__parser.parseRecord(__record);
+    __loadFromFields(__fields);
+  }
+
+  private void __loadFromFields(List<String> fields) {
+    Iterator<String> __it = fields.listIterator();
+    String __cur_str = null;
+    try {
+    __cur_str = __it.next();
+    if (__cur_str.equals("null") || __cur_str.length() == 0) { this.actor_id = null; } else {
+      this.actor_id = Integer.valueOf(__cur_str);
+    }
+
+    __cur_str = __it.next();
+    if (__cur_str.equals("null")) { this.first_name = null; } else {
+      this.first_name = __cur_str;
+    }
+
+    __cur_str = __it.next();
+    if (__cur_str.equals("null")) { this.last_name = null; } else {
+      this.last_name = __cur_str;
+    }
+
+    __cur_str = __it.next();
+    if (__cur_str.equals("null") || __cur_str.length() == 0) { this.last_update = null; } else {
+      this.last_update = java.sql.Timestamp.valueOf(__cur_str);
+    }
+
+    } catch (RuntimeException e) {    throw new RuntimeException("Can't parse input data: '" + __cur_str + "'", e);    }  }
+
+  private void __loadFromFields0(Iterator<String> __it) {
+    String __cur_str = null;
+    try {
+    __cur_str = __it.next();
+    if (__cur_str.equals("null") || __cur_str.length() == 0) { this.actor_id = null; } else {
+      this.actor_id = Integer.valueOf(__cur_str);
+    }
+
+    __cur_str = __it.next();
+    if (__cur_str.equals("null")) { this.first_name = null; } else {
+      this.first_name = __cur_str;
+    }
+
+    __cur_str = __it.next();
+    if (__cur_str.equals("null")) { this.last_name = null; } else {
+      this.last_name = __cur_str;
+    }
+
+    __cur_str = __it.next();
+    if (__cur_str.equals("null") || __cur_str.length() == 0) { this.last_update = null; } else {
+      this.last_update = java.sql.Timestamp.valueOf(__cur_str);
+    }
+
+    } catch (RuntimeException e) {    throw new RuntimeException("Can't parse input data: '" + __cur_str + "'", e);    }  }
+
+  public Object clone() throws CloneNotSupportedException {
+    actor o = (actor) super.clone();
+    o.last_update = (o.last_update != null) ? (java.sql.Timestamp) o.last_update.clone() : null;
+    return o;
+  }
+
+  public void clone0(actor o) throws CloneNotSupportedException {
+    o.last_update = (o.last_update != null) ? (java.sql.Timestamp) o.last_update.clone() : null;
+  }
+
+  public Map<String, Object> getFieldMap() {
+    Map<String, Object> __sqoop$field_map = new TreeMap<String, Object>();
+    __sqoop$field_map.put("actor_id", this.actor_id);
+    __sqoop$field_map.put("first_name", this.first_name);
+    __sqoop$field_map.put("last_name", this.last_name);
+    __sqoop$field_map.put("last_update", this.last_update);
+    return __sqoop$field_map;
+  }
+
+  public void getFieldMap0(Map<String, Object> __sqoop$field_map) {
+    __sqoop$field_map.put("actor_id", this.actor_id);
+    __sqoop$field_map.put("first_name", this.first_name);
+    __sqoop$field_map.put("last_name", this.last_name);
+    __sqoop$field_map.put("last_update", this.last_update);
+  }
+
+  public void setField(String __fieldName, Object __fieldVal) {
+    if ("actor_id".equals(__fieldName)) {
+      this.actor_id = (Integer) __fieldVal;
+    }
+    else    if ("first_name".equals(__fieldName)) {
+      this.first_name = (String) __fieldVal;
+    }
+    else    if ("last_name".equals(__fieldName)) {
+      this.last_name = (String) __fieldVal;
+    }
+    else    if ("last_update".equals(__fieldName)) {
+      this.last_update = (java.sql.Timestamp) __fieldVal;
+    }
+    else {
+      throw new RuntimeException("No such field: " + __fieldName);
+    }
+  }
+  public boolean setField0(String __fieldName, Object __fieldVal) {
+    if ("actor_id".equals(__fieldName)) {
+      this.actor_id = (Integer) __fieldVal;
+      return true;
+    }
+    else    if ("first_name".equals(__fieldName)) {
+      this.first_name = (String) __fieldVal;
+      return true;
+    }
+    else    if ("last_name".equals(__fieldName)) {
+      this.last_name = (String) __fieldVal;
+      return true;
+    }
+    else    if ("last_update".equals(__fieldName)) {
+      this.last_update = (java.sql.Timestamp) __fieldVal;
+      return true;
+    }
+    else {
+      return false;    }
+  }
+}
